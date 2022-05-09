8a56c367b4bbf918e596ef91443198c6a0671769
patrickctrf <patrickctrf@gmail.com>
2022-05-09 04:48:20 -0300

    feat: Dataset class created for Nsynth first version.

---
+++ E2/datasets.py
@@ -0,0 +1,76 @@
+import json
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+from scipy.io import wavfile
+from torch.utils.data import Dataset
+
+
+class NsynthDataset(Dataset):
+
+    def __init__(self, path="nsynth-valid/", shuffle=True):
+        super().__init__()
+
+        self.path = path
+
+        instr_fmly_name = ["bass", "brass", "flute", "guitar", "keyboard",
+                           "mallet", "organ", "reed", "string", "synth_lead",
+                           "vocal"]
+        instr_fmly_num = range(len(instr_fmly_name))  # 11 familias
+
+        self.instr_fmly_dict = dict(zip(instr_fmly_name, instr_fmly_num))
+
+        notas_nomes = ["do", "do_s", "re", "re_s", "mi", "fa", "fa_s", "sol",
+                       "sol_s", "la", "la_s", "si"]
+        self.notas_indices = range(len(notas_nomes))  # 12 seminotas
+
+        self.notas_dict = dict(zip(notas_nomes, self.notas_indices))
+
+        with open(os.path.join(path, 'examples.json'), 'r') as file:
+            summary_dict = json.load(file)
+
+        self.summary_df = pd.DataFrame(list(summary_dict.values()))
+        # self.summary_df = pd.get_dummies(self.summary_df, columns=["instrument_family_str", ])
+
+        self.shuffle_array = np.arange(self.summary_df.shape[0])
+        if shuffle is True: np.random.shuffle(self.shuffle_array)
+
+    def __getitem__(self, index):
+        idx = self.shuffle_array[index]
+
+        sample_info = self.summary_df.loc[idx]
+
+        sample_audio_array = wavfile.read(os.path.join(self.path, "audio", sample_info["note_str"]) + ".wav")[1]
+        sample_audio_array = self._scale_data(sample_audio_array.reshape(1, 1, -1))
+
+        intr_fmly_one_hot = torch.zeros((1, len(self.instr_fmly_dict.keys()), 64000))
+        notas_one_hot = torch.zeros((1, len(self.notas_indices), 64000))
+
+        # Setamos como 1 o elemento daquela familia (one hot)
+        intr_fmly_one_hot[0, self.instr_fmly_dict[sample_info["instrument_family_str"]]] = 1
+        # pitch dividido por 12 (qtde de notas), para transcrever em nota
+        # dentro da "oitava" em que aquele pitch se encontra.
+        notas_one_hot[0, self.notas_indices[sample_info["pitch"] % len(self.notas_indices)]] = 1
+
+        # Retorna X e Y:
+        # Sendo x == (ruido, one hot do timbre (family), one hot das notas)
+        # Sendo y == (sample_de_audio, one hot do timbre, one hot das notas)
+        return torch.cat((torch.normal(mean=0, std=0.5, size=(1, 1, 64000)), intr_fmly_one_hot, notas_one_hot), dim=1), \
+               torch.cat((torch.tensor(sample_audio_array), intr_fmly_one_hot, notas_one_hot), dim=1)
+
+    def __len__(self, ):
+        return self.summary_df.shape[0]
+
+    def _scale_data(self, array, means=0.0, stds=30000):
+        return (array - means) / stds
+
+    def _unscale_data(self, array, means=0.0, stds=30000):
+        return array * stds + means
+
+# dataset = NsynthDataset()
+#
+# dataset[0]
+#
+# x = 1
---
+++ E2/main.py
@@ -0,0 +1,53 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+def train(max_int: int = 128, batch_size: int = 16, training_steps: int = 500):
+    input_length = int(math.log(max_int, 2))
+
+    # Models
+    generator = Generator(input_length)
+    discriminator = Discriminator(input_length)
+
+    # Optimizers
+    generator_optimizer = torch.optim.Adam(generator.parameters(), lr=0.001)
+    discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001)
+
+    # loss
+    loss = nn.MSELoss()
+
+    for i in range(training_steps):
+        # zero the gradients on each iteration
+        generator_optimizer.zero_grad()
+
+        # Create noisy input for generator
+        # Need float type instead of int
+        noise = torch.randint(0, 2, size=(batch_size, input_length)).float()
+        generated_data = generator(noise)
+
+        # Generate examples of even real data
+        true_labels, true_data = generate_even_data(max_int, batch_size=batch_size)
+        true_labels = torch.tensor(true_labels).float()
+        true_data = torch.tensor(true_data).float()
+
+        # Train the generator
+        # We invert the labels here and don't train the discriminator because we want the generator
+        # to make things the discriminator classifies as true.
+        generator_discriminator_out = discriminator(generated_data)
+        generator_loss = loss(generator_discriminator_out, true_labels)
+        generator_loss.backward()
+        generator_optimizer.step()
+
+        # Train the discriminator on the true/generated data
+        discriminator_optimizer.zero_grad()
+        true_discriminator_out = discriminator(true_data)
+        true_discriminator_loss = loss(true_discriminator_out, true_labels)
+
+        # add .detach() here think about this
+        generator_discriminator_out = discriminator(generated_data.detach())
+        generator_discriminator_loss = loss(generator_discriminator_out, torch.zeros(batch_size))
+        discriminator_loss = (true_discriminator_loss + generator_discriminator_loss) / 2
+        discriminator_loss.backward()
+        discriminator_optimizer.step()

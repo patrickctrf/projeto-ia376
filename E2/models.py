55aad4335650adaa901550839d96c372b70caa97
patrickctrf <patrickctrf@gmail.com>
2022-05-21 03:01:52 -0300

    fix: Passing categorical info to discriminator. Remove bn from discrim

--- E2/models.py
+++ E2/models.py
@@ -2,44 +2,22 @@ from torch import nn
 from torch.nn import Sequential, Conv1d, Linear, AdaptiveAvgPool1d, Sigmoid, AdaptiveMaxPool1d
 
 
-class Generator1DUpsampled(nn.Module):
-    def __init__(self, noise_length=256, target_length=64000, n_input_channels=24, n_output_channels=64,
-                 kernel_size=7, stride=1, padding=0, dilation=1,
-                 bias=False):
-        super().__init__()
-        self.target_length = target_length
-
-        n_filters = 32
-
-        self.feature_generator = Sequential(
-            nn.Upsample(size=(32000,), mode='linear', align_corners=True),
-            nn.Conv1d(n_input_channels, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
-            nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
-            nn.Upsample(size=(64004,), mode='linear', align_corners=True),
-            nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
-            nn.Conv1d(256, n_output_channels, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias),
-        )
-
-    def forward(self, x):
-        return self.feature_generator(x).view(-1, 1, self.target_length)
-
-
 class Discriminator1D(nn.Module):
     def __init__(self, seq_length=64000, n_input_channels=24,
                  kernel_size=7, stride=1, padding=0, dilation=1, bias=False):
         super().__init__()
 
-        n_output_channels = 256
+        n_output_channels = 64
 
         self.feature_extractor = Sequential(
-            nn.BatchNorm1d(n_input_channels, affine=False),
-            nn.Conv1d(n_input_channels, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            # nn.BatchNorm1d(n_input_channels, affine=False),
+            nn.Conv1d(n_input_channels, n_output_channels, kernel_size=(3,), stride=(3,), dilation=(2,), bias=bias), nn.Tanh(),
             nn.MaxPool1d(2, 2),
-            nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            nn.Conv1d(n_output_channels, n_output_channels, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
             nn.MaxPool1d(2, 2),
-            nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            nn.Conv1d(n_output_channels, n_output_channels, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
             nn.MaxPool1d(2, 2),
-            nn.Conv1d(256, n_output_channels, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            nn.Conv1d(n_output_channels, n_output_channels, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
         )
 
         self.pooling = AdaptiveMaxPool1d(1)
@@ -59,40 +37,6 @@ class Discriminator1D(nn.Module):
     def forward(self, x):
         return self.activation(self.linear(self.pooling(self.feature_extractor(x)).flatten(start_dim=1)))
         # return self.aux(x)
-
-
-class ResBlock(nn.Module):
-    def __init__(self, n_input_channels=6, n_output_channels=7,
-                 kernel_size=7, stride=1, padding=0, dilation=1,
-                 groups=1, bias=True, padding_mode='replicate'):
-        """
-    ResNet-like block, receives as arguments the same that PyTorch's Conv1D
-    module.
-        """
-        super(ResBlock, self).__init__()
-
-        self.feature_extractor = \
-            Sequential(
-                Conv1d(n_input_channels, n_output_channels, kernel_size,
-                       stride, kernel_size // 2 * dilation, dilation,
-                       groups, bias, padding_mode),
-                nn.LeakyReLU(),
-                nn.BatchNorm1d(n_output_channels),
-                Conv1d(n_output_channels, n_output_channels, kernel_size,
-                       stride, kernel_size // 2 * dilation,
-                       dilation, groups, bias, padding_mode),
-                nn.PReLU(num_parameters=n_output_channels),
-                nn.BatchNorm1d(n_output_channels)
-            )
-
-        self.skip_connection = \
-            Sequential(
-                Conv1d(n_input_channels, n_output_channels, (1,),
-                       stride, padding, dilation, groups, bias, padding_mode)
-            )
-
-    def forward(self, input_seq):
-        return self.feature_extractor(input_seq) + self.skip_connection(input_seq)
 
 
 class Generator1DTransposed(nn.Module):
@@ -138,6 +82,62 @@ class Generator1DTransposed(nn.Module):
             nn.ConvTranspose1d(8 * n_filters, 9 * n_filters, kernel_size=(4,), stride=(2,), padding=(1,), bias=bias),  # L=64000
             nn.Tanh(),
             nn.Conv1d(9 * n_filters, 1, (1,), bias=bias),  # L=64000
+        )
+
+    def forward(self, x):
+        return self.feature_generator(x).view(-1, 1, self.target_length)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, n_input_channels=6, n_output_channels=7,
+                 kernel_size=7, stride=1, padding=0, dilation=1,
+                 groups=1, bias=True, padding_mode='replicate'):
+        """
+    ResNet-like block, receives as arguments the same that PyTorch's Conv1D
+    module.
+        """
+        super(ResBlock, self).__init__()
+
+        self.feature_extractor = \
+            Sequential(
+                Conv1d(n_input_channels, n_output_channels, kernel_size,
+                       stride, kernel_size // 2 * dilation, dilation,
+                       groups, bias, padding_mode),
+                nn.LeakyReLU(),
+                nn.BatchNorm1d(n_output_channels),
+                Conv1d(n_output_channels, n_output_channels, kernel_size,
+                       stride, kernel_size // 2 * dilation,
+                       dilation, groups, bias, padding_mode),
+                nn.PReLU(num_parameters=n_output_channels),
+                nn.BatchNorm1d(n_output_channels)
+            )
+
+        self.skip_connection = \
+            Sequential(
+                Conv1d(n_input_channels, n_output_channels, (1,),
+                       stride, padding, dilation, groups, bias, padding_mode)
+            )
+
+    def forward(self, input_seq):
+        return self.feature_extractor(input_seq) + self.skip_connection(input_seq)
+
+
+class Generator1DUpsampled(nn.Module):
+    def __init__(self, noise_length=256, target_length=64000, n_input_channels=24, n_output_channels=64,
+                 kernel_size=7, stride=1, padding=0, dilation=1,
+                 bias=False):
+        super().__init__()
+        self.target_length = target_length
+
+        n_filters = 32
+
+        self.feature_generator = Sequential(
+            nn.Upsample(size=(32000,), mode='linear', align_corners=True),
+            nn.Conv1d(n_input_channels, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            nn.Upsample(size=(64004,), mode='linear', align_corners=True),
+            nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias), nn.Tanh(),
+            nn.Conv1d(256, n_output_channels, kernel_size=(3,), stride=(1,), dilation=(1,), bias=bias),
         )
 
     def forward(self, x):

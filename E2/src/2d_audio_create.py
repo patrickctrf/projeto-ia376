3f16e975aa881292d66eb6eeccaa6216d09eb517
patrickctrf <patrickctrf@gmail.com>
2022-05-24 15:48:06 -0300

    refactor: New directory structure.

---
+++ E2/src/2d_audio_create.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+import sounddevice as sd
+from scipy import signal
+
+
+def polar_to_rect(amplitude, angle):
+    return amplitude * (np.cos(angle) + 1j * np.sin(angle))
+
+
+generator = torch.load("checkpoints/best_generator.pth").to(torch.device("cpu"))
+
+ruido_e_classes = torch.cat(
+    (
+        torch.normal(mean=0, std=1.0, size=(9, 1)),
+        torch.tensor([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, ]).view(-1, 1),
+        torch.tensor([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]).view(-1, 1),
+    ), dim=0).view(1, 32, 1, 1)
+
+fourier_sintetizado = generator(ruido_e_classes)[0].detach().numpy()
+
+espectro = polar_to_rect(10 ** fourier_sintetizado[0], fourier_sintetizado[1])
+
+t, x = signal.istft(Zxx=espectro, fs=2046, nperseg=2046, noverlap=3 * 2046 // 4, )
+
+x = x.astype(np.int16)
+
+sd.play(x, samplerate=16000, blocking=True)
